2f9472504c64260829a82a4d8cfb529dbf52b953
Scott McMurray <scottmcm@users.noreply.github.com>
2017-10-28 04:33:40 -0700

    Use fold instead of for_each for min/max

--- src/libcore/iter/iterator.rs
+++ src/libcore/iter/iterator.rs
@@ -2351,17 +2351,17 @@ fn select_fold1<I, B, FProj, FCmp>(mut it: I,
     // start with the first element as our selection. This avoids
     // having to use `Option`s inside the loop, translating to a
     // sizeable performance gain (6x in one case).
-    it.next().map(|mut sel| {
-        let mut sel_p = f_proj(&sel);
+    it.next().map(|first| {
+        let first_p = f_proj(&first);
 
-        it.for_each(|x| {
+        it.fold((first_p, first), |(sel_p, sel), x| {
             let x_p = f_proj(&x);
             if f_cmp(&sel_p, &sel, &x_p, &x) {
-                sel = x;
-                sel_p = x_p;
+                (x_p, x)
+            } else {
+                (sel_p, sel)
             }
-        });
-        (sel_p, sel)
+        })
     })
 }
 
